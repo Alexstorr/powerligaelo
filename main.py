f9db2b8155a4c684c8fa48434b20e344b0cc8b41
Alexstorr <48597341+Alexstorr@users.noreply.github.com>
2024-04-21 00:31:10 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,62 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import pandas as pd
+import os
+
+api_key = '45fc023a-12ed-45df-a6a2-3ae080eaad03'
+
+r=requests.get("https://liga.dust2.dk/hold/?hold=61239a41191993590c361e9d")
+soup=BeautifulSoup(r.text, "html.parser")
+
+ids = re.findall(r'id=(\d+)', str(soup.find_all("img", class_="players--player-img")))
+nickname_divs = soup.find_all("div", class_="players--player--nickname text-ellipsis")
+nicknames = [div.text.strip() for div in nickname_divs]
+teamname_divs = soup.find_all("div", class_="menu--teamName")
+teamnames = [div.text.strip() for div in teamname_divs]
+teamlogo_divs = soup.find_all("img", class_="menu--team--img")
+teamlogos = [div["src"] for div in teamlogo_divs]
+team_ids = []
+value_ids = []
+league_ids = []
+divs = soup.find_all("div", class_="players--player")
+for div in divs:
+    team_id = div.get("teamid")
+    if team_id:
+        team_ids.append(team_id)
+for div in divs:
+    value_id = div.get("value")
+    if value_id:
+        value_ids.append(value_id)
+for div in divs:
+    league_id = div.get("leagueid")
+    if league_id:
+        league_ids.append(league_id)
+
+dic={}
+id=0
+team=0
+n=len(nicknames)
+for i in range(n):
+    if nicknames[i]=="TBA":
+        dic[("TBA")+str(i)]=((str(nicknames[i])+str(i)), 0, 0, teamnames[team], teamlogos[team])
+        id = id-1
+        print("TBA")
+    else:
+        url = f'https://open.faceit.com/data/v4/players?game=cs2&game_player_id={ids[id]}'
+
+        headers = {
+            'Accept': 'application/json',
+            'Authorization': 'Bearer ' + api_key
+        }
+        response = requests.get(url, headers=headers)
+        faceitdata = response.json()
+
+        try: dic[faceitdata["nickname"]]=(nicknames[i], faceitdata["games"]["cs2"]["skill_level"], faceitdata["games"]["cs2"]["faceit_elo"], teamnames[team], teamlogos[team])
+        except: dic["Ingen Faceit"]=(nicknames[i], 0, 0, teamnames[team], teamlogos[team])
+    id = id + 1
+    if (i+1) % 5 == 0:
+        team += 1
+df = pd.DataFrame(dic)
+os.remove("dic.csv")
+df.to_csv("dic.csv")
